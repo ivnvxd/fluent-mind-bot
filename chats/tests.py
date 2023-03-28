9737c83a3c24aee620666caa6835b9d03f790956
Andrey Ivanov <ivnv.xd@gmail.com>
2023-03-28 18:21:11 +0200

    add basic test

--- chats/tests.py
+++ chats/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse_lazy
 
-# Create your tests here.
+
+class TestListMessages(TestCase):
+    def test_tasks_view(self) -> None:
+        self.client = Client()
+
+        response = self.client.get(reverse_lazy('chats'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            template_name='messages.html'
+        )
