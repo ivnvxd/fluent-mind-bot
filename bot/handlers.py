318da54f08616f68c00a5044b0ff4942aae915dc
Andrey Ivanov <ivnv.xd@gmail.com>
2023-04-01 22:27:17 +0200

    refactor: move functions to helpers.py; fix linter errors

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,85 +1,33 @@
 import openai
-from asgiref.sync import sync_to_async
 
 from django.utils import timezone
-from chats.models import Text, Chat
 
 from telegram import Update
-from telegram.ext import Application, MessageHandler, CommandHandler, filters, CallbackContext, ContextTypes, PicklePersistence
+from telegram.ext import CallbackContext, ContextTypes
 from telegram.constants import ParseMode, ChatAction
 
-from .helpers import send_action
+from .helpers import send_action, get_messages_count, get_or_create_chat, \
+    get_first_sentence, get_conversation_history, create_message_entry, \
+    save_chat, delete_chat
 
 
 HELP_MESSAGE = """Available commands:
 🔄 /retry — Regenerate last answer (🚧)
-✨ /new — Start new chat (🚧)
+✨ /new — Start new chat
 📝 /history — Show previous chats (🚧)
 💾 /save — Save current chat (🚧)
 ❓ /help — Show help
 """
-
-system = {"role": "system", "content": "You are a helpful assistant."}
-
-
-@sync_to_async
-def get_or_create_chat(telegram_id, create_new_chat=False):
-    if create_new_chat:
-        chat = Chat.objects.create(telegram_id=telegram_id)
-    else:
-        chat = Chat.objects.filter(telegram_id=telegram_id).order_by('-creation_date').first()
-        if not chat:
-            chat = Chat.objects.create(telegram_id=telegram_id)
-    return chat
-
-
-@sync_to_async
-def get_messages_count(telegram_id, chat):
-    return Text.objects.filter(telegram_id=telegram_id, chat=chat).count()
-
-
-@sync_to_async
-def create_message_entry(chat, **kwargs):
-    text = Text.objects.create(chat=chat, **kwargs)
-    return text
-
-
-@sync_to_async
-def get_conversation_history(telegram_id, chat):
-    messages = Text.objects.filter(telegram_id=telegram_id, chat=chat).order_by('date')
-
-    request = [system]
-    for message in messages:
-        request.extend([
-            {"role": "user", "content": message.request},
-            {"role": "assistant", "content": message.response}
-        ])
-
-    return request
-
-
-@sync_to_async
-def save_chat(chat):
-    chat.save()
-
-
-@sync_to_async
-def delete_chat(chat):
-    chat.delete()
-
-
-async def get_first_sentence(response_text):
-    first_sentence = response_text.split('.')[0].strip()
-    return first_sentence
 
 
 @send_action(ChatAction.TYPING)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Start the bot."""
 
-    text = "🤖 Hi! I'm <b>ChatGPT</b> bot implemented with GPT-3.5 OpenAI API 🤖\n\n"
-    text += HELP_MESSAGE
-    text += "\nAnd now... ask me anything!"
+    text = "🤖 Hi! I'm <b>ChatGPT</b> bot " + \
+           "implemented with GPT-3.5 OpenAI API 🤖\n\n" + \
+           HELP_MESSAGE + \
+           "\nAnd now... ask me anything!"
 
     await update.message.reply_text(text, parse_mode=ParseMode.HTML)
 
@@ -87,7 +35,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def help(update: Update, context: CallbackContext):
     """Show available commands."""
 
-    await context.bot.send_message(chat_id=update.message.chat_id, text=HELP_MESSAGE, parse_mode=ParseMode.HTML)
+    await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text=HELP_MESSAGE,
+        parse_mode=ParseMode.HTML
+    )
 
 
 async def new(update: Update, context: CallbackContext):
@@ -99,9 +51,12 @@ async def new(update: Update, context: CallbackContext):
     if messages_count == 0:
         await delete_chat(current_chat)
 
-    chat = await get_or_create_chat(telegram_id, create_new_chat=True)
+    await get_or_create_chat(telegram_id, create_new_chat=True)
 
-    text = f"Let's start over.\n\nYou can always go back to previous conversations with the /history command."
+    text = "Let's start over.\n\n" + \
+           "You can always go back to previous conversations" + \
+           "with the /history command."
+
     await update.message.reply_text(text)
 
 
@@ -115,7 +70,10 @@ async def unknown(update: Update, context: CallbackContext):
 
 @send_action(ChatAction.TYPING)
 async def echo(update: Update, context: CallbackContext):
-    await context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+    await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text=update.message.text
+    )
 
 
 @send_action(ChatAction.TYPING)
@@ -150,7 +108,7 @@ async def chat(update: Update, context: CallbackContext):
         chat.summary = first_sentence[:1000]
         await save_chat(chat)
 
-    text_entry = await create_message_entry(
+    await create_message_entry(
         chat=chat,
         telegram_id=telegram_id,
         username=username,
@@ -160,7 +118,7 @@ async def chat(update: Update, context: CallbackContext):
         prompt_tokens=prompt_tokens,
     )
 
-    chat.last_update = timezone.now()  # Update the last_update field of the Chat model
+    chat.last_update = timezone.now()
     await save_chat(chat)
 
     print(update.message)
--- bot/helpers.py
+++ bot/helpers.py
@@ -1,4 +1,10 @@
 from functools import wraps
+from asgiref.sync import sync_to_async
+
+from chats.models import Text, Chat
+
+
+system = {"role": "system", "content": "You are a helpful assistant."}
 
 
 def send_action(action):
@@ -12,7 +18,60 @@ def send_action(action):
                 action=action
             )
             return await func(update, context, *args, **kwargs)
-
         return command_func
-
     return decorator
+
+
+@sync_to_async
+def get_or_create_chat(telegram_id, create_new_chat=False):
+    if create_new_chat:
+        chat = Chat.objects.create(telegram_id=telegram_id)
+    else:
+        chat = Chat.objects.filter(
+            telegram_id=telegram_id
+        ).order_by('-creation_date').first()
+        if not chat:
+            chat = Chat.objects.create(telegram_id=telegram_id)
+    return chat
+
+
+@sync_to_async
+def get_messages_count(telegram_id, chat):
+    return Text.objects.filter(telegram_id=telegram_id, chat=chat).count()
+
+
+@sync_to_async
+def create_message_entry(chat, **kwargs):
+    text = Text.objects.create(chat=chat, **kwargs)
+    return text
+
+
+@sync_to_async
+def get_conversation_history(telegram_id, chat):
+    messages = Text.objects.filter(
+        telegram_id=telegram_id, chat=chat
+    ).order_by('date')
+
+    request = [system]
+    for message in messages:
+        request.extend([
+            {"role": "user", "content": message.request},
+            {"role": "assistant", "content": message.response}
+        ])
+
+    return request
+
+
+@sync_to_async
+def save_chat(chat):
+    chat.save()
+
+
+@sync_to_async
+def delete_chat(chat):
+    chat.delete()
+
+
+async def get_first_sentence(response_text):
+    first_sentence = response_text.split('.')[0].strip()
+    return first_sentence
